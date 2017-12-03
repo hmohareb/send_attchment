b578cd03f963774bb72c7faf627542eb655a7fdf
Hmood <hamadmohareb@gmail.com>
2017-12-03 14:05:40 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,46 +4,46 @@
 //send_mail($from,$to,$subject,$body_email,$path_attchment,$reply_to)
 function send_mail($from,$to,$subject,$body_email,$path_attchment,$reply_to,$mail_send,$mail_failed)   {
 
-	$random_hash = md5(date('r', time()));
-	//define the headers we want passed. Note that they are separated with \r\n
-	$headers = "From: " . $from . "\r\nReply-To:". $reply_to ;
-	//add boundary string and mime type specification
-	$headers .= "\r\nContent-Type: multipart/mixed; boundary=\"PHP-mixed-".$random_hash."\"";
-	//read the atachment file contents into a string,
-	//encode it with MIME base64, 
-	//and split it into smaller chunks
-	$attachment = chunk_split(base64_encode(file_get_contents($path_attchment)));
-	//define the body of the message.
-	ob_start(); //Turn on output buffering
-	?>
-	--PHP-mixed-<?php echo $random_hash; ?> 
-	Content-Type: multipart/alternative; boundary="PHP-alt-<?php echo $random_hash; ?>"
+$random_hash = md5(date('r', time()));
+//define the headers we want passed. Note that they are separated with \r\n
+$headers = "From: " . $from ."\r\nReply-To: " . $reply_to ;
+//add boundary string and mime type specification
+$headers .= "\r\nContent-Type: multipart/mixed; boundary=\"PHP-mixed-".$random_hash."\"";
+//read the atachment file contents into a string,
+//encode it with MIME base64,
+//and split it into smaller chunks
+$attachment = chunk_split(base64_encode(file_get_contents($path_attchment)));
+//define the body of the message.
+ob_start(); //Turn on output buffering
+?>
+--PHP-mixed-<?php echo $random_hash; ?> 
+Content-Type: multipart/alternative; boundary="PHP-alt-<?php echo $random_hash; ?>"
 
-	--PHP-alt-<?php echo $random_hash; ?> 
-	Content-Type: text/html; charset="iso-8859-1"
-	Content-Transfer-Encoding: 7bit
+--PHP-alt-<?php echo $random_hash; ?> 
+Content-Type: text/html; charset="iso-8859-1"
+Content-Transfer-Encoding: 7bit
 
-	<?php print $body_email ; ?>
+<?php echo $body_email ;?>
 
-	--PHP-alt-<?php echo $random_hash; ?>--
+--PHP-alt-<?php echo $random_hash; ?>--
 
-	--PHP-mixed-<?php echo $random_hash; ?> 
-	Content-Type: <?php echo get_mime_type($path_attchment); ?>; name="attachment" 
-	Content-Transfer-Encoding: base64 
-	Content-Disposition: attachment 
+--PHP-mixed-<?php echo $random_hash; ?> 
+Content-Type: <?php echo get_mime_type($path_attchment); ?> ; name="logo.png" 
+Content-Transfer-Encoding: base64  
+Content-Disposition: attachment 
 
-	<?php echo $attachment; ?>
-	--PHP-mixed-<?php echo $random_hash; ?>--
+<?php echo $attachment; ?>
+--PHP-mixed-<?php echo $random_hash; ?>--
 
-	<?php
-	//copy current buffer contents into $message variable and delete current output buffer
-	$message = ob_get_clean();
-	//send the email
-	$mail_sent = @mail( $to, $subject, $message, $headers );
-	//if the message is sent successfully print "Mail sent". Otherwise print "Mail failed"
-	echo $mail_sent ? $mail_send : $mail_failed;
+<?php
+//copy current buffer contents into $message variable and delete current output buffer
+$message = ob_get_clean();
+//send the email
+$mail_sent = mail( $to, $subject, $message, $headers ); 
+//if the message is sent successfully print "Mail sent". Otherwise print "Mail failed"
+echo $mail_sent ? $mail_send : $mail_failed ;
+
 }
-
 
 //This function ti Select the attachment file type
 function get_mime_type($filename) {
